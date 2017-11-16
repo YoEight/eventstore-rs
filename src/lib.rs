5ab23d003fe81080579c89b2e42b1c32d8ae4015
YoEight <yo.eight@gmail.com>
2017-11-16 22:22:43 +0100

    Move to chan crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,17 @@
+extern crate chan;
 extern crate core;
 extern crate bytes;
-extern crate futures;
-extern crate tokio_core;
-extern crate tokio_io;
-extern crate two_lock_queue;
 extern crate uuid;
 
 use core::option::Option;
 use core::result::Result;
-use std::io::{ Error, ErrorKind, Cursor };
+use std::io::{ Error, Cursor };
 use std::net::{ TcpStream, SocketAddrV4 };
 use std::thread::{ spawn, JoinHandle };
 
 use bytes::{ Buf, BytesMut, LittleEndian };
 use bytes::buf::BufMut;
-use futures::{ Future, Sink, Stream };
-use tokio_core::reactor::Core;
-use tokio_io::codec::{ Encoder, Decoder };
-use tokio_io::AsyncRead;
-use two_lock_queue::{ Sender, Receiver, unbounded };
+use chan::{ Sender, Receiver, async };
 use uuid::Uuid;
 
 pub struct Pkg {
@@ -47,46 +40,46 @@ impl Pkg {
     }
 }
 
-pub struct PkgCodec;
-
-impl Encoder for PkgCodec {
-    type Item  = Pkg;
-    type Error = Error;
-
-    fn encode(&mut self, item: Pkg, dest: &mut BytesMut) -> Result<(), Error> {
-        dest.put_u32::<LittleEndian>(item.size());
-        dest.put_u8(item.cmd);
-        dest.put_u8(0); // Package credential flag.
-        dest.put_slice(item.correlation.as_bytes());
-
-        Result::Ok(())
-    }
-}
-
-impl Decoder for PkgCodec {
-    type Item  = Pkg;
-    type Error = Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Pkg>, Error> {
-        // Checks if the frame size has been sent.
-        if src.len() < 4 {
-            Result::Ok(Option::None)
-        } else {
-            let mut frame_cursor = Cursor::new(src.split_to(3));
-            let frame            = frame_cursor.get_u32::<LittleEndian>() as usize;
-
-            // Checks if all the message payload has been sent.
-            if src.len() < frame {
-                Result::Ok(Option::None)
-            } else {
-                let cmd         = src[0];
-                let correlation = Uuid::from_bytes(&src[2..18]).unwrap();
-
-                Result::Ok(Option::Some(Pkg::new(cmd, correlation)))
-            }
-        }
-    }
-}
+// pub struct PkgCodec;
+//
+// impl Encoder for PkgCodec {
+//     type Item  = Pkg;
+//     type Error = Error;
+//
+//     fn encode(&mut self, item: Pkg, dest: &mut BytesMut) -> Result<(), Error> {
+//         dest.put_u32::<LittleEndian>(item.size());
+//         dest.put_u8(item.cmd);
+//         dest.put_u8(0); // Package credential flag.
+//         dest.put_slice(item.correlation.as_bytes());
+//
+//         Result::Ok(())
+//     }
+// }
+//
+// impl Decoder for PkgCodec {
+//     type Item  = Pkg;
+//     type Error = Error;
+//
+//     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Pkg>, Error> {
+//         // Checks if the frame size has been sent.
+//         if src.len() < 4 {
+//             Result::Ok(Option::None)
+//         } else {
+//             let mut frame_cursor = Cursor::new(src.split_to(3));
+//             let frame            = frame_cursor.get_u32::<LittleEndian>() as usize;
+//
+//             // Checks if all the message payload has been sent.
+//             if src.len() < frame {
+//                 Result::Ok(Option::None)
+//             } else {
+//                 let cmd         = src[0];
+//                 let correlation = Uuid::from_bytes(&src[2..18]).unwrap();
+//
+//                 Result::Ok(Option::Some(Pkg::new(cmd, correlation)))
+//             }
+//         }
+//     }
+// }
 
 enum Msg {
     Start,
@@ -107,7 +100,7 @@ struct Connection {
 
 impl Connection {
     fn new(bus: Sender<Msg>, addr: SocketAddrV4) -> Connection {
-        let (sender, recv) = unbounded();
+        let (sender, recv) = async();
         let id             = Uuid::new_v4();
         let worker         = spawn(move || Connection::create_conn(id, recv, bus, addr));
 
@@ -121,13 +114,13 @@ impl Connection {
     fn create_conn(id: Uuid, rx: Receiver<Pkg>, bus: Sender<Msg>, addr: SocketAddrV4) {
         let stream = TcpStream::connect(addr).unwrap();
 
-        bus.send(Msg::Established(id)).unwrap();
+        bus.send(Msg::Established(id));
     }
 }
 
 impl Client {
     pub fn new(addr: SocketAddrV4) -> Client {
-        let (sender, recv) = unbounded();
+        let (sender, recv) = async();
         let tx             = sender.clone();
         let handle         = spawn(move || Client::worker_thread(addr, tx, recv));
 
@@ -138,30 +131,37 @@ impl Client {
     }
 
     fn worker_thread(addr: SocketAddrV4, bus: Sender<Msg>, queue: Receiver<Msg>) {
-        let mut keepGoing  = true;
+        let mut keep_going  = true;
         let mut connection = Option::None;
         let mut connected  = false;
 
-        while keepGoing {
-            let msg = queue.recv().unwrap();
+        while keep_going {
+            let msg_opt = queue.recv();
 
-            match msg {
-                Msg::Start => {
-                    connection = Option::Some(Connection::new(bus.clone(), addr));
-                },
+            match msg_opt {
+                Option::Some(msg) => match msg {
+                    Msg::Start => {
+                        connection = Option::Some(Connection::new(bus.clone(), addr));
+                    },
 
-                Msg::Shutdown => {
-                    keepGoing = false;
-                    println!("Shutting down...");
-                },
+                    Msg::Shutdown => {
+                        keep_going = false;
+                        println!("Shutting down...");
+                    },
 
-                Msg::Established(id) => {
-                    for conn in &connection {
-                        if conn.id == id {
-                            connected = true;
+                    Msg::Established(id) => {
+                        for conn in &connection {
+                            if conn.id == id {
+                                connected = true;
+                            }
                         }
-                    }
+                    },
                 },
+
+                Option::None => {
+                    println!("Main bus closed");
+                    keep_going = false;
+                }
             }
         }
     }
