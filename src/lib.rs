bc66db7c815b28024f365146f41b116b883bab06
YoEight <yo.eight@gmail.com>
2017-11-17 23:14:37 +0100

    Parse a Pkg from a TcpStream

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@ extern crate bytes;
 extern crate uuid;
 
 use core::option::Option;
-use core::result::Result;
-use std::io::{ Error, Cursor };
+use std::io::{ Cursor, Read };
 use std::net::{ TcpStream, SocketAddrV4 };
 use std::thread::{ spawn, JoinHandle };
 
@@ -39,7 +38,7 @@ impl Pkg {
         }
     }
 
-    pub fn toBytes(&self) -> BytesMut {
+    pub fn to_bytes(&self) -> BytesMut {
         // FIXME - Use with_capacity instead.
         let mut bytes = BytesMut::new();
 
@@ -51,11 +50,19 @@ impl Pkg {
         bytes
     }
 
-    pub fn from_bytes(src: &mut BytesMut) -> Pkg {
-        let mut frame_cursor = Cursor::new(src.split_to(3));
-        let     frame        = frame_cursor.get_u32::<LittleEndian>() as usize;
-        let     cmd          = src[0];
-        let     correlation  = Uuid::from_bytes(&src[2..18]).unwrap();
+    pub fn from_stream(stream: &mut TcpStream) -> Pkg {
+        let mut frame: [u8; 4] = [0; 4];
+
+        stream.read_exact(&mut frame).unwrap();
+
+        let mut frame_cursor = Cursor::new(frame);
+        let     frame_size   = frame_cursor.get_u32::<LittleEndian>() as usize;
+        let mut pkg_buf      = vec![0; frame_size];
+
+        stream.read_exact(&mut pkg_buf).unwrap();
+
+        let cmd         = pkg_buf[0];
+        let correlation = Uuid::from_bytes(&pkg_buf[2..18]).unwrap();
 
         Pkg::new(cmd, correlation)
     }
