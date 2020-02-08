2df42d10ace1752951d2fb68cca12aee3ab61fd9
YoEight <yo.eight@gmail.com>
2020-02-09 00:13:07 +0100

    Fix es6 module documentation.

--- src/es6/mod.rs
+++ src/es6/mod.rs
@@ -1,3 +1,5 @@
+//! [PREVIEW] Exposes API to communicate with ES 6 server. ES 6 Server is still in development so
+//! expect that interface to change. Enable `es6` feature flag to use it.
 pub mod commands;
 pub mod connection;
 pub mod types;
