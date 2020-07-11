0400de10ac32591b188807a486b8fc7d27303da4
Denis Tsai <denistsai@aetheras.io>
2020-07-10 23:23:33 -0700

    derive Clone for connections

--- src/es6/connection.rs
+++ src/es6/connection.rs
@@ -33,6 +33,7 @@ impl rustls::ServerCertVerifier for NoVerification {
 /// or a single thread can make many asynchronous requests. To get the most
 /// performance out of the connection, it is generally recommended to use it
 /// in this way.
+#[derive(Clone)]
 pub struct Connection {
     settings: Settings,
     streams: streams::streams_client::StreamsClient<Channel>,
